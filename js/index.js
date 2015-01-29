af8b0380fff9f102684bd4ca0ce50caa657bffe5
Nignesh <NiGi@Nigneshs-MacBook-Pro.local>
2015-01-29 06:38:28 -0500

    Added
    - Custom Alert msg
    - Draw image in canvas

--- js/index.js
+++ js/index.js
@@ -1,61 +1,94 @@
+var canvas, output,errorDiv;
 document.addEventListener("DOMContentLoaded", function () {
-	
-	var btn = document.createElement("BUTTON");        
-		var t = document.createTextNode("Get My Location");       
-		btn.appendChild(t);                        
-		btn.className="btn";
-		btn.addEventListener("click",getLocation);
-		document.body.appendChild(btn);
-		
-	
+
+	//Create button
+	var btn = document.createElement("BUTTON");
+	var t = document.createTextNode("Get My Location");
+	btn.appendChild(t);
+	btn.className = "btn";
+	btn.addEventListener("click", getLocation);
+	document.body.appendChild(btn);
 });
 
-function getLocation()
-{
-	//Create container div
-	var output = document.createElement("div");
-	output.setAttribute("class","output");
-	document.body.appendChild(output);
+function getLocation() {
 	
-	//Browser does not support geolocation API
-  if (!navigator.geolocation){
-    //Error message pop up
-	var div = document.createElement("div");
-	div.setAttribute("id","err_dialog");
-	div.innerHTML = "Sorry, but your browser does not support location based awesomeness.";
-	div.style.display="none";
-	document.body.appendChild(div);
-	  
-    return;
-  }
+	//Create container div
+	output=document.querySelector(".output");
+	if(!output)
+	{
+		output = document.createElement("div");
+		output.setAttribute("class", "output");
+		document.body.appendChild(output);
+	}
+
+	//if browser does not support geolocation API
+	if (!navigator.geolocation) {
+		//Error message pop up
+		errorDiv=document.querySelector("#err_dialog");
+		if(!errorDiv)
+		{
+			errorDiv = document.createElement("div");
+			errorDiv.setAttribute("id", "err_dialog");
+			document.body.appendChild(errorDiv);
+		}
+		errorDiv.innerHTML = "Sorry, but your browser does not support location based awesomeness.";
+		//set timeout for error msg
+		setTimeout(function(){
+		   errorDiv.style.display = 'none';
+		}, 3000);  //3secs
+		
+		return;
+	}
 
 	//got the location
-  function success(position) {
-    var latitude  = position.coords.latitude;
-    var longitude = position.coords.longitude;
+	function success(position) {
+		var latitude = position.coords.latitude;
+		var longitude = position.coords.longitude;
+		//output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+		document.querySelector("#locating") = "";
 
-//    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+		//Draw Image
+		canvas=document.querySelector(".canvas");
+		if(!canvas)
+		{
+			canvas = document.createElement("canvas");
+			canvas.setAttribute("class", "canvas");
+			//canvas.style="width: 400px";
+			document.querySelector('.output').appendChild(canvas);
+		}
+		
+		var ctx = canvas.getContext('2d');
+		var img = new Image();
+		img.onload = function () {
+			ctx.drawImage(img, 0, 0, img.width, img.height);
+		};
 
-    var img = new Image();
-    img.src = "http://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=14&size=400x400&sensor=false&&markers=color:blue%7Clabel:S";
+		img.src = "http://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=14&size=400x400&sensor=false&markers=color:red%7Clabel:C%" + latitude + "," + longitude + "";
 
-	 output.innerHTML="";
-    output.appendChild(img);
-	  
-  };
+	};
 
 	//Error while getting location
-  function error() {
-	var div = document.createElement("div");
-	div.setAttribute("id","err_dialog");
-	div.innerHTML = "Unable to retrieve your location.";
-	div.style.display="none";
-	document.body.appendChild(div);
-  };
+	function error() {
+		document.querySelector("#locating").innerHTML = "";
+		errorDiv=document.querySelector("#err_dialog");
+		if(!errorDiv)
+		{
+			errorDiv = document.createElement("div");
+			errorDiv.setAttribute("id", "err_dialog");
+			document.body.appendChild(errorDiv);
+		}
+		errorDiv.innerHTML = "Unable to retrieve your location.";
+		
+		//set timeout for error msg
+		setTimeout(function(){
+		   errorDiv.style.display = 'none';
+		}, 3000);  //3secs
+		
+	};
 
-  output.innerHTML = "<p style='text-align: center'>Locating…</p>";
+	output.innerHTML = "<p id='locating' style='text-align: center'>Locating…</p>";
 
 	//get current position
-  navigator.geolocation.getCurrentPosition(success, error);
-	
+	navigator.geolocation.getCurrentPosition(success, error);
+
 }
